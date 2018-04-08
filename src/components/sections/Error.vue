cef117cef5a8bd2042f32498331952fb1c7f949e
limoer <myt19960503@outlook.com>
2018-04-08 22:12:23 +0800

    增加加载和错误组件

---
+++ src/components/sections/Error.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="error-container">
+    <p class="alert">加载出错了...</p>
+    <img src="../../assets/error.png" width="100%" height="243px" alt="加载错误">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Erro'
+}
+</script>
+
+<style scoped>
+.error-container {
+  position: absolute;
+  top: 0;
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0,0,0,.1);
+  z-index: -10;
+}
+.alert {
+  margin: 0;
+  margin-top: 4rem;
+  font-weight: bold;
+  color: #fff;
+}
+</style>
---
+++ src/components/sections/Loading.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="loading-container">
+    <p>加载中...</p>
+    <van-loading style="margin-left: 40%;" type="circle" color="black" size="60px"/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Loading'
+}
+</script>
+
+<style scoped>
+.loading-container {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .2);
+  z-index: 10;
+}
+.loading-container > p {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  margin-top: 5rem;
+  text-align: center;
+  color: rgba(0, 0, 0, .4);
+  font-weight: bold;
+}
+</style>
